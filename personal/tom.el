b5636c40fecfae3d5adfb5514e9656428f653517
Tom Paton <tom.paton@gmail.com>
2013-03-24 23:22:36 +0100

    save-excursion in toggle-quotes so cursor doesn't move.

--- personal/tom.el
+++ personal/tom.el
@@ -109,23 +109,24 @@
   flip the internal quotes as well.  Best to run on the first
   character of the string."
   (interactive)
-  (re-search-backward "[\"']")
-  (let* ((start (point))
-         (old-c (char-after start))
-         new-c)
-    (setq new-c 
-          (case old-c
-            (?\" "'")
-            (?\' "\"")))
-    (setq old-c (char-to-string old-c))
-    (delete-char 1)
-    (insert new-c)
-    (re-search-forward old-c)
-    (backward-char 1)
-    (let ((end (point)))
+  (save-excursion
+    (re-search-backward "[\"']")
+    (let* ((start (point))
+           (old-c (char-after start))
+           new-c)
+      (setq new-c 
+            (case old-c
+              (?\" "'")
+              (?\' "\"")))
+      (setq old-c (char-to-string old-c))
       (delete-char 1)
       (insert new-c)
-      (replace-string new-c old-c nil (1+ start) end))))
+      (re-search-forward old-c)
+      (backward-char 1)
+      (let ((end (point)))
+        (delete-char 1)
+        (insert new-c)
+        (replace-string new-c old-c nil (1+ start) end)))))
 (global-set-key (kbd "C-c '") 'toggle-quotes)
 
 ;; helm
