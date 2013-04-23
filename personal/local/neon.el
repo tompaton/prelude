37a9ea3df4b668b54cdce2349a13e80455b0ec12
Tom Paton <tom.paton@gmail.com>
2013-04-23 21:58:09 +1000

    bind C-c ? to autoclose django tags

--- personal/local/neon.el
+++ personal/local/neon.el
@@ -47,3 +47,35 @@
 (set-face-attribute 'mmm-code-submode-face nil :background nil)
 (set-face-attribute 'mmm-output-submode-face nil :background"#EEEEFF")
 (set-face-attribute 'mmm-declaration-submode-face nil :background"#EEFFFF")
+
+;; auto-close django template tags
+(defvar django-closable-tags
+  '("for" "block" "comment" "filter" "ifchanged" "ifequal"
+    "ifnotequal" "spaceless" "if"))
+
+(defvar django-tag-re
+  (concat "{%\\s *\\(end\\)?\\("
+          (mapconcat 'identity django-closable-tags "\\|")
+          "\\)[^%]*%}"))
+
+(defun django-find-open-tag ()
+  (if (search-backward-regexp django-tag-re nil t)
+      (if (match-string 1) ; If it's an end tag
+          (if (not (string= (match-string 2) (django-find-open-tag)))
+              (error "Unmatched Django tag")
+            (django-find-open-tag))
+        (match-string 2)) ; Otherwise, return the match
+    nil))
+
+(defun django-close-tag ()
+  (interactive)
+  (let ((open-tag (save-excursion (django-find-open-tag))))
+    (if open-tag
+        (insert "{% end" open-tag " %}")
+      (error "Nothing to close"))))
+
+;; C-c ? is sgml-tag-help, change so it's like C-c / (sgml-close-tag)
+(eval-after-load "sgml-mode"
+  '(progn
+     (define-key html-mode-map
+       "\C-c?" 'django-close-tag)))
