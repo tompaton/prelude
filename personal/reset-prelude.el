aca1b8cd586f480d6da88e44662d87d9907229aa
Tom Paton <tom.paton@gmail.com>
2013-04-23 15:34:21 +1000

    unbind C-x C-c

--- personal/reset-prelude.el
+++ personal/reset-prelude.el
@@ -7,6 +7,9 @@
 ;; C-c k prelude-kill-other-buffers is easily mistyped when going for C-x k
 (put 'prelude-kill-other-buffers 'disabled t)
 
+;; quitting by mistake is really annoying
+(global-set-key (kbd "C-x C-c") nil)
+
 ;; no point hamstringing myself by hiding the menu
 (menu-bar-mode t)
 
